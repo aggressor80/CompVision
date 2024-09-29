893aec8937d46dbdcd2d590d561f97426a0adda3
aggressor80 <romario080806@gmail.com>
2024-09-29 22:35:38 +0300

    laba

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array1 = np.array([1, 2, 3, 4, 5])
+array2 = np.array([5, 4, 3, 2, 1])
+
+product_array = array1 * array2
+
+print(array1)
+print(array2)
+print(product_array)
