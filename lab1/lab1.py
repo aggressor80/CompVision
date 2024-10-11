4b8275f2de7932fafb511e9490873d3458deb7f0
aggressor80 <160965538+aggressor80@users.noreply.github.com>
2024-10-11 14:54:25 +0300

    Add files via upload

---
+++ lab1/lab1.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('photos/text2.jpg')
+height, width, channels = img.shape
+
+# Конвертация изображения в черно-белое
+def black_white(img):
+    bw_img = np.zeros((height, width), dtype=np.uint8)
+    for i in range(height):
+        for j in range(width):
+            b, g, r = img[i][j]
+            gray = (b + g + r) / 3
+            bw_img[i][j] = gray
+    return bw_img
+
+# Метод Отсу
+def otsu_method(bw_img):
+    H = np.zeros(256)
+    for i in range(height):
+        for j in range(width):
+            one_pixel = bw_img[i, j]
+            H[one_pixel] = H[one_pixel] + 1
+
+    probabilities_list = H/(height * width)
+    variances_list = []
+    for i in range(0, 256):
+        q_1 = 0
+        for x in range(i):
+            q_1 = q_1 + probabilities_list[x]
+
+        q_2 = 0
+        for x in range(i + 1, 256):
+            q_2 = q_2 + probabilities_list[x]
+
+        if q_1 == 0 or q_2 == 0: continue
+
+        avg1 = 0
+        for x in range(i):
+            avg1 = avg1 + (x * probabilities_list[x]) / q_1
+
+        avg2 = 0
+        for x in range(i + 1, 256):
+            avg2 = avg2 + (x * probabilities_list[x]) / q_2
+
+        variance_1 = 0
+        for x in range(i):
+            variance_1 = variance_1 + ((x - avg1) ** 2) * (probabilities_list[x] / q_1)
+        variance_1 = variance_1 ** 2
+
+        variance_2 = 0
+        for x in range(i + 1, 256):
+            variance_2 = variance_2 + ((x - avg2) ** 2) * (probabilities_list[x] / q_2)
+        variance_2 = variance_2 ** 2
+
+        variances_list.append((q_1 * variance_1 + q_2 * variance_2) ** 2)
+        min_variance = min(variances_list)
+        t_val = variances_list.index(min_variance)
+
+    return t_val
+
+# Создание бинарной маски для изображения
+def make_mask(t, blacknwhite_img):
+    for i in range(height):
+        for j in range(width):
+            if blacknwhite_img[i, j] > t:
+                blacknwhite_img[i, j] = 0
+            else:
+                blacknwhite_img[i, j] = 1
+
+    return bw_img
+
+bw_img = black_white(img)
+t = otsu_method(bw_img)
+img_mask = np.array(make_mask(t, bw_img) * 255, dtype=np.uint8)
+m_color = cv2.cvtColor(img_mask, cv2.COLOR_GRAY2BGR)
+mask_img = cv2.bitwise_and(img, m_color)
+cv2.imwrite('final5.png', mask_img)
+
